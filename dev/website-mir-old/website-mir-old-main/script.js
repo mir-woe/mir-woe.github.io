d958984c326c15a1fe8502aa8995147ea2807772
mir-miri <76711137+mir-miri@users.noreply.github.com>
2021-10-01 17:00:05 +0100

    yessir

---
+++ dev/website-mir-old/website-mir-old-main/script.js
@@ -0,0 +1,45 @@
+function uwu()
+        {
+        var x = document.getElementById("fuckinginsertion")
+    x.innerHTML = '<style>.shinigami{display:initial;}</style>';}
+    
+    
+    
+
+
+
+    
+    
+    
+function oldie()
+        {
+        var x = document.getElementById("killkill")
+    x.innerHTML = '<style>.shinigami{margin:0px !important;}  .poiuyhtred{display:none;} .oiuyhgt{display:flex;} .dea{display:none;}</style>';}
+
+function poiuyhtred()
+        {
+        var x = document.getElementById("killkill")
+    x.innerHTML = '<style>.shinigami{margin:0px !important;}  .poiuyhtred{display:flex;} .oiuyhgt{display:none;} .dea{display:none;}</style>';}
+    
+function dea()
+        {
+        var x = document.getElementById("killkill")
+    x.innerHTML = '<style>.shinigami{margin:0px !important;}  .poiuyhtred{display:none;} .oiuyhgt{display:none;} .dea{display:flex;}</style>';}
+  
+    
+    
+
+
+
+    
+    
+    
+function kujuu()
+        {
+               var x = document.getElementById("lokl")
+    x.innerHTML = '<style>.ujuu{ box-shadow: none; border-radius: none; border: none; background: none; background-size: none;}</style>';}
+
+function owo() 
+        {
+               var x = document.getElementById("pills")
+    x.innerHTML = '<div id="fuckinginsertion"></div><div id="killkill"></div><div id="lokl"></div>';}
