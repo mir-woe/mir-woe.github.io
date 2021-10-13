8657c5ecf8f8131aba1cdf01f870738af7af54f9
mir-miri <76711137+mir-miri@users.noreply.github.com>
2021-10-13 19:12:48 +0100

    reorganisation blah nlah  edfv

--- footer.js
+++ footer.js
@@ -5,7 +5,7 @@ function myFooters() {
 
     var footer = document.createElement("FOOTER");
     footer.setAttribute("id", "bottom");
-    footer.innerHTML = '<button style="display: inline-block;" id="expand-btn" onclick="myFootersExpand()">menu</button> <div style="display: none; border-bottom: solid #2d2e30 !important" id="shrink-btn"> <button onclick="myFootersShrink()">&darr;</button> <button> ididd </button> </div>    <div id="noshow"><p id="leftist">&#169; 2020 - <span id="cpyear">n</span> ikhlas / All rights reserved.</p> <p><a id="rightist" target=blank href="https://github.com/mir-miri/mir-miri.github.io">source code</a></p></div>';
+    footer.innerHTML = '<button style="display: inline-block;" id="expand-btn" onclick="myFootersExpand()">menu</button> <div style="display: none; border-bottom: solid #2d2e30 !important" id="shrink-btn"> <button onclick="myFootersShrink()">&darr;</button>   <a href="https://mir-woe.github.io/home/home.html"><button>home</button></a>     <a href="https://mir-woe.github.io/dev/profile.html"><button> about </button></a>  <a href="https://mir-woe.github.io/dev/list.html"><button> respositories </button></a>  <a href="https://mir-woe.github.io/lost.html"><button>lost?</button></a></div>    <div id="noshow"><p id="leftist">&#169; 2020 - <span id="cpyear">n</span> woe / All rights reserved.</p> <p><a id="rightist" target=blank href="https://github.com/mir-woe/mir-woe.github.io">source code</a></p></div>';
     document.body.appendChild(footer);
 
     document.getElementById("cpyear").textContent = y;
