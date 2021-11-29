9d1a99da51172fb42d21bb74ab186cf82fcaadef
mir-miri <76711137+mir-miri@users.noreply.github.com>
2021-11-29 17:44:40 +0000

    a commit bcs keeping it here is compiling

--- misc/JSbs/basicinfo.js
+++ misc/JSbs/basicinfo.js
@@ -1,4 +1,3 @@
-
 var gitlink = "https://github.com/mir-woe"
 var tglink = "https://t.me/LeMiri"
 var spotlink = "https://open.spotify.com/user/jcjbe02uxml6wion3hjg6ss3p"
@@ -6,17 +5,22 @@ var ytlink = "https://www.youtube.com/channel/UCb-ILQ2PHkvyM9xxzNv7Q-Q/featured"
 var ttvlink = "https://www.twitch.tv/woe_ikhlas"
 var cashlink = "https://cash.app/%C2%A3hungryikhlas"
 var paylink = "https://paypal.me/hungrymiri"
+var gdevlink = "https://developers.google.com/profile/u/mir_woe"
 
 
 var list = {
     ownername : "woe",
-    webversion : "v3.2.1",
+    webversion : "v3.4.0",
     gitname : "@mir-woe",
+    gdevname : "@mir-woe",
     ttvname : "@woe_ikhlas",
     ytname : "@ιкнļαѕ",
     spotname : "@MīR",
     discname : "@woe#1863",
-    tgname : "@LeMiri"
+    tgname : "@LeMiri",
+    revoltname : "@mir_woe",
+    ppname : "@hungrymiri",
+    caname : "£hungryikhlas"
 };
 
 for (var value in list) {
