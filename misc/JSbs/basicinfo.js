28c17b0fe149e5867d4e491928982af0503a248d
mir-miri <76711137+mir-miri@users.noreply.github.com>
2022-03-01 01:00:10 +0000

    website

--- misc/JSbs/basicinfo.js
+++ misc/JSbs/basicinfo.js
@@ -3,14 +3,14 @@ var tglink = "https://t.me/LeMiri"
 var spotlink = "https://open.spotify.com/user/jcjbe02uxml6wion3hjg6ss3p"
 var ytlink = "https://www.youtube.com/channel/UCb-ILQ2PHkvyM9xxzNv7Q-Q/featured"
 var ttvlink = "https://www.twitch.tv/woe_ikhlas"
-var cashlink = "https://cash.app/%C2%A3hungryikhlas"
+var cashlink = "https://cash.app/hungryikhlas"
 var paylink = "https://paypal.me/hungrymiri"
 var gdevlink = "https://developers.google.com/profile/u/mir_woe"
 
 
 var list = {
     ownername: "woe",
-    webversion: "v3.6.1",
+    webversion: "v3.6.2",
     gitname: "@mir-woe",
     gdevname: "@mir-woe",
     ttvname: "@woe_ikhlas",
